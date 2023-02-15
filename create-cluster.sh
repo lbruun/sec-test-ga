94f2f0c353cebe46fb3f0a2046b316c3de530d7c
Lars Bruun-Hansen <lars.bruun-hansen@mail.dk>
2023-02-15 17:16:46 +0100

    Create create-cluster.sh

---
+++ create-cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+printf "CREATING KIND CLUSTER\n-----------------------------------------------------------\n"
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+EOF
+
+printf "\nKUBECTL GET ALL\n-----------------------------------------------------------\n"
+kubectl get all
